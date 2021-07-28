8a60438ab85ed106b796b8c28a087840cf419af7
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-07-29 00:09:07 +0200

    update

--- article/views.py
+++ article/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-     return render(request, 'index.html')
+     return render(request, 'index.html',{'number':[1,2,3,4,5]})
+
+def about(request):
+     return render(request, 'about.html')     
