6e19b7f5fe45c114f10a30b12c5334777dd7d9e9
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-08-10 00:19:08 +0200

    update

--- article/views.py
+++ article/views.py
@@ -6,4 +6,8 @@ def index(request):
      return render(request, 'index.html',{'number':[1,2,3,4,5]})
 
 def about(request):
-     return render(request, 'about.html')     
+     return render(request, 'about.html')  
+
+def detail(request,id):
+     return HttpResponse('Detail:' +str(id))  
+
