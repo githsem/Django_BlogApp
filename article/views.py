faac177bc2dc735fe1e2f47fb26b11cdbbb2b314
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-08-12 23:35:22 +0200

    update

--- article/views.py
+++ article/views.py
@@ -9,8 +9,5 @@ def about(request):
      return render(request, 'about.html')  
 
 def detail(request,id):
-     return HttpResponse('Detail:' +str(id))  
-
-def article(request):
-     return render(request, 'article.html')       
+     return HttpResponse('Detail:' +str(id)) 
 
