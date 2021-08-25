ad07c661d2069827bbab919e999d7b10e9065702
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-08-25 21:39:23 +0200

    update

--- user/views.py
+++ user/views.py
@@ -1,15 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import RegisterForm
+
+from django.contrib.auth.models import User
+from django.contrib.auth import login
 
 # Create your views here.
 
 def register(request):
-    form = RegisterForm()
-    context = {
-        "form" : form
-    }
+    if request.method == 'POST' :
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username= form.cleaned_data.get("username")
+            password= form.cleaned_data.get("password")
 
-    return render(request, "register.html",context)
+            newUser = User(username = username)
+            newUser.set_password(password)
+
+            newUser.save()
+            login(request,newUser)
+
+            return redirect("index")
+        context = {
+            "form" : form
+        }
+
+        return render(request, "register.html",context)    
+
+
+
+    else:
+        form = RegisterForm()
+        context = {
+        "form" : form
+        }
+
+        return render(request, "register.html",context)
+    # form = RegisterForm()
+    # context = {
+    #     "form" : form
+    # }
+
+    # return render(request, "register.html",context)
 
 def loginUser(request):
     return render(request, "login.html")
