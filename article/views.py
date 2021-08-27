9557739f097a7270d8d79cf766037a0f23abc7c3
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-08-28 00:43:35 +0200

    update

--- article/views.py
+++ article/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from .forms import ArticleForm
+from django.contrib import messages
+from .models import Article
 
 
 # Create your views here.
@@ -10,4 +13,26 @@ def about(request):
 
 def detail(request,id):
      return HttpResponse('Detail:' +str(id)) 
+
+def dashboard(request):
+     articles = Article.objects.filter(author=request.user)
+     context ={
+          "articles":articles
+     }
+
+     return render(request, 'dashboard.html',context)     
+
+def addArticle(request):
+     form = ArticleForm(request.POST or None)
+
+     if form.is_valid():
+          article = form.save(commit=False)
+
+          article.author = request.user
+          article.save()
+          messages.success(request, "Article created successfully")
+          return redirect("index")
+
+
+     return render(request, 'addarticle.html',{"form":form})        
 
