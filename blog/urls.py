cee4584e61b53a5db5bb3c38ff37e8be78eef3d2
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-08-31 00:42:33 +0200

    update

--- blog/urls.py
+++ blog/urls.py
@@ -15,6 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 from article import views
 
 urlpatterns = [
@@ -24,3 +28,4 @@ urlpatterns = [
     path('user/',include("user.urls")),
      
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
