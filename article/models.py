5ad4cc36f1a0ec535770a92f33d03b6da732cb84
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-08-30 22:10:04 +0200

    update

--- article/models.py
+++ article/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 
 class Article(models.Model):
     author = models.ForeignKey("auth.User", on_delete= models.CASCADE)
     titel = models.CharField(max_length=50)
-    content = models.TextField()
+    content = RichTextField()
     created_date = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.titel
