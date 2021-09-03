18af0e15cc885d323fa60d01bc81fae2dfd81443
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-09-03 12:45:14 +0200

    update

--- blog/urls.py
+++ blog/urls.py
@@ -24,6 +24,7 @@ from article import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index,name='index'), 
+    path('about/', views.about,name='about'), 
     path('articles/',include("article.urls")),
     path('user/',include("user.urls")),
      
