439fbd1b92541349bfdc84b0c2f73bc391808c4c
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-09-03 13:27:30 +0200

    update

--- article/models.py
+++ article/models.py
@@ -11,3 +11,11 @@ class Article(models.Model):
     article_image = models.FileField(blank=True, null=True,verbose_name="Add Picture")
     def __str__(self):
         return self.titel
+
+class Comment(models.Model):   
+    article = models.ForeignKey(Article, on_delete = models.CASCADE, verbose_name="Article", related_name="comments")   
+    comment_author = models.CharField(max_length=50, verbose_name="Name")
+    comment_content = models.CharField(max_length=200, verbose_name="Comment")
+    comment_date = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.comment_content
