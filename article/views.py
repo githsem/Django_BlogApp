1badb8a1f3adf1e0cf2a66385ad563c748564f83
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-09-06 16:58:07 +0200

    update

--- article/views.py
+++ article/views.py
@@ -9,11 +9,9 @@ from django.contrib.auth.decorators import login_required
 
 def articles(request):
      keyword = request.GET.get("keyword")
-
      if keyword:
           articles = Article.objects.filter(titel__contains = keyword)
           return render(request, "articles.html",{"articles":articles})
-
      articles = Article.objects.all()
      return render(request, "articles.html",{"articles":articles})
 
@@ -32,47 +30,38 @@ def dashboard(request):
      context ={
           "articles":articles
      }
-
      return render(request, 'dashboard.html',context)     
 
 @login_required(login_url="user:login")
 def addArticle(request):
      form = ArticleForm(request.POST or None, request.FILES or None)
-
      if form.is_valid():
           article = form.save(commit=False)
-
           article.author = request.user
           article.save()
           messages.success(request, "Article created successfully")
           return redirect("article:dashboard")
-
      return render(request, 'addarticle.html',{"form":form})   
 
 def detail(request,id):   
      article = get_object_or_404(Article,id=id)
-
      comments = article.comments.all()
      return render(request, 'detail.html',{"article":article,"comments":comments})   
 
 @login_required(login_url="user:login")
 def updateArticle(request,id):   
-
      article = get_object_or_404(Article,id=id)
      form = ArticleForm(request.POST or None, request.FILES or None,instance = article) 
      if form.is_valid():
           article = form.save(commit=False)
-
           article.author = request.user
           article.save()
           messages.success(request, "Article updated successfully")
           return redirect("article:dashboard")
-
      return render(request, "update.html",{"form":form})
 
 @login_required(login_url="user:login")
 def deleteArticle(request,id):   
-
      article = get_object_or_404(Article,id=id) 
      article.delete()    
      messages.success(request, "Article deleted successfully")
@@ -80,13 +69,10 @@ def deleteArticle(request,id):
 
 def addComment(request,id):
      article = get_object_or_404(Article,id=id)
-
      if request.method == 'POST':
           comment_author = request.POST.get("comment_author")
           comment_content = request.POST.get("comment_content")  
-
           newComment = Comment(comment_author=comment_author, comment_content=comment_content) 
-
           newComment.article = article
           newComment.save()   
      return redirect(reverse("article:detail",kwargs={"id":id}))     
